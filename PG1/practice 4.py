88aa0d4c5295ea1fa7d347ca109390a11abe1a3a
uwu <tcheredeeva.dasha@gmail.com>
2023-11-24 20:07:46 +0300

    fourth exercise

--- PG1/practice 4.py
+++ PG1/practice 4.py
@@ -3,17 +3,19 @@ import random
 
 
 if __name__ == '__main__':
-    n, k = map(int, input().split())
+    w, n = map(int, input().split())
 
     pygame.init()
 
-    size = width, height = 2 * n * k, 2 * n * k
+    size = width, height = w, w
     screen = pygame.display.set_mode(size)
     xc, yc = width // 2, height // 2
-    colors = ('blue', 'red', 'green')
+    screen.fill('white')
     # drawing...
-    for i in range(k, 0, -1):
-        pygame.draw.circle(screen, colors[i % 3], (xc, yc), n * i)
+    for r in range(n):
+        for c in range(n):
+            if (r + c) % 2 == 0:
+                screen.fill('black', pygame.Rect(c * (w // n), r * (w // n), w // n, w // n))
 
     pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
